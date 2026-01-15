3eac840d0da89ae6bd35a18dbccbd00c2e5c70de
Rafael Niepce <rniepce@gmail.com>
2026-01-15 10:38:32 -0300

    Fix langchain import error and pin version

--- backend.py
+++ backend.py
@@ -1,7 +1,7 @@
 import os
 from typing import List
 from langchain_community.document_loaders import PyPDFLoader, Docx2txtLoader, TextLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
 from langchain.docstore.document import Document
